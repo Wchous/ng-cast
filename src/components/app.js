26ff58e8827709cceb674f1f1037ca873f86257d
wchous <wchoustoun@gmail.com>
2018-06-30 17:05:29 -0500

    good API in  progress

--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,7 @@ var app = angular.module('video-player', [])
   .component('app', {
     templateUrl: 'src/templates/app.html',
     controller: function() {
+      
       this.videos = window.exampleVideoData;
       
       this.selectVideo = function(video){
@@ -18,6 +19,17 @@ var app = angular.module('video-player', [])
       this.nowplaying = this.videos[0];
       this.selectVideo = this.selectVideo.bind(this);
       
+      // youTube
+      // this.searchResults = function(query){
+      //   var context = this
+      //   youTube.search(query, function(response){context.videos = response.data.items; context.selectVideo(context.videos[0])})
+        
+        
+      // }
+      
+      // youTube(query, (res) => {
+      //   console.log(res)
+      // })
     
     }
   });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,11 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
-});
+  .service('youTube', function($http){
+    $http.get(`https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=5`, {
+      params: {
+        part: 'snippet',
+        q: query,
+        maxResults: 5 ,
+        key: window.YOUTUBE_API_KEY 
+      }
+    }).then(console.log('we are awesome'));
+  });
